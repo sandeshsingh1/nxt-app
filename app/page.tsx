688de716f573bc3901dba2423f991f08c45ced83
Sandesh SIngh <sandeshsingh201106@gmail.com>
2026-01-14 18:45:54 +0530

    Added headiing of my homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,11 @@
 import React from 'react'
-
 const page = () => {
   return (
-    <div>     
-  <h1>Welcome</h1>
-
-
-
-
-    </div>
+   <section>
+    <h1 className='text-center'>The Hub for Every Dev <br /> Event You cant Miss
+    </h1>
+    <p className='text-center mt-5'>Hackathons, Meetups and Conferences All in One Place</p>
+   </section>
   )
 }
-
 export default page
