669620df1439fcdf32a1ca7abd139aad0719e441
Sandesh SIngh <sandeshsingh201106@gmail.com>
2026-01-16 01:00:54 +0530

    Added date-time for events

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -9,10 +9,25 @@ interface Props{
   date:string;
   time:string;
 }
-const EventCard = ({title,image}:Props) => {
+const EventCard = ({title,image,slug,location,date,time}:Props) => {
   return (
-    <Link href={'/event'} id="event-card">
+    <Link href={'/event/${slug}'} id="event-card">
       <Image src={image} alt={title} width={410} height={300}    className="poster"/>
+      <div className='flex flex-row gap-2'>
+        <Image src="/icons/pin.svg" alt="location" width={14} height={14}/>
+        <p>{location}</p>
+        <div className='date-time'>
+          <div>
+            <Image src="/icons/calendar.svg" alt="date" width={14} height={14}/>
+            <p>{date}</p>
+          </div>
+             <div>
+            <Image src="/icons/clock.svg" alt="time" width={14} height={14}/>
+            <p>{time}</p>
+          </div>
+
+        </div>
+      </div>
       <p className='title'>{title}</p>
     </Link>
   )
