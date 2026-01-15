aa5c86c1616244d8f1bc807caabe3463f4ced168
Sandesh SIngh <sandeshsingh201106@gmail.com>
2026-01-16 00:23:54 +0530

    Added Event

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
+import EventCard from '@/components/EventCard'
 import ExploreBtn from '@/components/ExploreBtn'
 import React from 'react'
+const events=[
+   {image :'/images/event1.png',title:'Event 1'},
+     {image :'/images/event2.png',title:'Event 2'},
+    
+]
 const page = () => {
   return (
    <section>
@@ -10,8 +16,11 @@ const page = () => {
     <div className='mt-20 space-y-7'>
       <h3>Featured Events</h3>
       <ul className='events'>
-        {[1,2,3,4,5].map((event)=>(
-          <li key={event}>Event {event}</li>
+        {events.map((event)=>(
+          <li key={event.title}>
+            <EventCard {...event}/>
+          </li>
+         
         ))}
       </ul>
     </div>
---
+++ components/EventCard.tsx
@@ -0,0 +1,17 @@
+import  Image  from 'next/image';
+import  Link  from 'next/link';
+import React from 'react'
+interface Props{
+  title:string;
+  image:string;
+}
+const EventCard = ({title,image}:Props) => {
+  return (
+    <Link href={'/event'} id="event-card">
+      <Image src={image} alt={title} width={410} height={300}    className="poster"/>
+      <p className='title'>{title}</p>
+    </Link>
+  )
+}
+
+export default EventCard
