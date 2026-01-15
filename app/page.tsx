9e60869a4b0dbf738b1a64c5afc9194201fbca3b
Sandesh SIngh <sandeshsingh201106@gmail.com>
2026-01-15 23:50:38 +0530

    Adding Events

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,14 @@ const page = () => {
     </h1>
     <p className='text-center mt-5'>Hackathons, Meetups and Conferences All in One Place</p>
     <ExploreBtn/>
+    <div className='mt-20 space-y-7'>
+      <h3>Featured Events</h3>
+      <ul className='events'>
+        {[1,2,3,4,5].map((event)=>(
+          <li key={event}>Event {event}</li>
+        ))}
+      </ul>
+    </div>
    </section>
   )
 }
